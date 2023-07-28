2441d26d8895decf5f3a66b1edb8017b336470aa
NessTal <tal.ness1@gmail.com>
2023-07-28 09:34:14 -0400

    Minor changes

--- get_features.py
+++ get_features.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import itertools
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -24,36 +25,47 @@ CONTOUS_INDEXES = list(set(itertools.chain(*mp_face_mesh.FACEMESH_CONTOURS)))
 
 INDEXES = LEFT_EYE_INDEXES + RIGHT_EYE_INDEXES + LEFT_IRIS_INDEXES + RIGHT_IRIS_INDEXES + CONTOUS_INDEXES
 
+file = 'face_video.mp4'
 
-video = cv2.VideoCapture('face_video.mp4')
+def get_features(file):
+    video = cv2.VideoCapture(file)
 
-points_over_time = []
-ret = True
-reduce_frame_rate = video.get(cv2.CAP_PROP_FPS)/50   # every 20 milliseconds
-i = 0
+    points_over_time = []
+    ret = True
+    reduce_frame_rate = video.get(cv2.CAP_PROP_FPS)/50   # every 20 milliseconds
+    i = 0
 
-while ret:
-    if i % reduce_frame_rate == 0:
-        ret, frame = video.read()
+    while ret:
+        if i % reduce_frame_rate == 0:
+            ret, frame = video.read()
 
-        if not ret:
-            break
+            if not ret:
+                break
 
-        image_height, image_width, _ = frame.shape
-        face_mesh_results = face_mesh_images.process(frame[:,:,::-1])
-        points = np.array([np.multiply([p.x, p.y], [image_width, image_height]).astype(int) for p in face_mesh_results.multi_face_landmarks[0].landmark])
-        points[INDEXES]
-        points_over_time.append((i,points[INDEXES]))
+            image_height, image_width, _ = frame.shape
+            face_mesh_results = face_mesh_images.process(frame[:,:,::-1])
+            points = np.array([np.multiply([p.x, p.y], [image_width, image_height]).astype(int) for p in face_mesh_results.multi_face_landmarks[0].landmark])
+            points[INDEXES]
+            points_over_time.append((i,points[INDEXES]))
 
-        i += 1
+            i += 1
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-    else:
-        ret = video.grab()
-        i += 1
+        else:
+            ret = video.grab()
+            i += 1
 
-video.release()
-#video.destroyAllWindows()
+    video.release()
+    #video.destroyAllWindows()
+    
+    return points_over_time
+
+
+
+df = pd.read_excel('EyeCodingResults.xlsx')
+df.to_parquet('EyeCodingResults.parquet')
+df.to_parquet('EyeCodingResults_cleaned.parquet')
+df = pd.read_parquet('EyeCodingResults_cleaned.parquet')
