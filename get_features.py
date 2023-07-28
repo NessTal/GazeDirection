b42a1cb37f1ea9fd938c0ac84ba0049b2f731948
NessTal <tal.ness1@gmail.com>
2023-07-28 19:36:44 -0400

    Load hand coded data, loop on video files to get landmarks

--- get_features.py
+++ get_features.py
@@ -4,17 +4,17 @@ import itertools
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
 
 mp_face_mesh = mp.solutions.face_mesh
-
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 face_mesh_images = mp_face_mesh.FaceMesh(static_image_mode=True, 
                                          refine_landmarks=True,
                                          max_num_faces=1,
                                          min_detection_confidence=0,
                                          min_tracking_confidence=0)
-
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 
 
 LEFT_EYE_INDEXES = list(set(itertools.chain(*mp_face_mesh.FACEMESH_LEFT_EYE)))
@@ -25,14 +25,22 @@ CONTOUS_INDEXES = list(set(itertools.chain(*mp_face_mesh.FACEMESH_CONTOURS)))
 
 INDEXES = LEFT_EYE_INDEXES + RIGHT_EYE_INDEXES + LEFT_IRIS_INDEXES + RIGHT_IRIS_INDEXES + CONTOUS_INDEXES
 
-file = 'face_video.mp4'
+
+files = os.listdir('video_files')
+
+hand_coded_df = pd.read_parquet('EyeCodingResults_cleaned.parquet')
+# hand_coded_df = hand_coded_df.loc[hand_coded_df['Time'] % 20 == 0] # downsample to 20ms (use if needed)
+
 
 def get_features(file):
     video = cv2.VideoCapture(file)
 
     points_over_time = []
     ret = True
-    reduce_frame_rate = video.get(cv2.CAP_PROP_FPS)/50   # every 20 milliseconds
+    fps = video.get(cv2.CAP_PROP_FPS)
+    reduce_frame_rate = int(fps/30)   # every 30 milliseconds (unless fps is less than 30)
+    if reduce_frame_rate < 1:
+        reduce_frame_rate = 1
     i = 0
 
     while ret:
@@ -46,13 +54,13 @@ def get_features(file):
             face_mesh_results = face_mesh_images.process(frame[:,:,::-1])
             points = np.array([np.multiply([p.x, p.y], [image_width, image_height]).astype(int) for p in face_mesh_results.multi_face_landmarks[0].landmark])
             points[INDEXES]
-            points_over_time.append((i,points[INDEXES]))
+            points_over_time.append((file,i*fps,points[INDEXES]))
 
             i += 1
 
             cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #    break
 
         else:
             ret = video.grab()
@@ -60,12 +68,25 @@ def get_features(file):
 
     video.release()
     #video.destroyAllWindows()
-    
+    points_over_time = pd.DataFrame(points_over_time, columns=['File','Time','Points'])
     return points_over_time
 
 
+points_over_time_df = pd.DataFrame(columns=['File','Time','Points'])
+for file in files[:10]:
+    if file.endswith('.mp4'):
+        points_over_time = get_features('video_files/'+file)
+        points_over_time_df = pd.concat([points_over_time_df,points_over_time])
 
-df = pd.read_excel('EyeCodingResults.xlsx')
-df.to_parquet('EyeCodingResults.parquet')
-df.to_parquet('EyeCodingResults_cleaned.parquet')
-df = pd.read_parquet('EyeCodingResults_cleaned.parquet')
+
+
+
+
+
+
+
+
+#df = pd.read_excel('EyeCodingResults.xlsx')
+#df.to_parquet('EyeCodingResults.parquet')
+#df.to_parquet('EyeCodingResults_cleaned.parquet')
+#df = pd.read_parquet('EyeCodingResults_cleaned.parquet')
